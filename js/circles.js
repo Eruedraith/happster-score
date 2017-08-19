99338aa9aa7b52883f24f829219fa18f8c474a9a
Eruedraith <eruedraith777@gmail.com>
2017-08-18 20:32:37 -0400

    favicon added, review cards styled, revieew section

--- js/circles.js
+++ js/circles.js
@@ -1,11 +1,11 @@
 var myCircle = Circles.create({
   id:                  'circles-1',
-  radius:              60,
+  radius:              70,
   value:               43,
   maxValue:            100,
   width:               10,
   text:                function(value){return value + '%';},
-  colors:              ['#D3B6C6', '#4B253A'],
+  colors:              ['#7FDBDF', '#54C5C9'],
   duration:            400,
   wrpClass:            'circles-wrp',
   textClass:           'circles-text',
