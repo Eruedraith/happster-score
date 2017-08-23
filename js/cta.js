706cc2886f031494f5b686fe2377301e91765c2a
Eruedraith <eruedraith777@gmail.com>
2017-08-23 15:05:32 -0400

    cta on results page for desktop and ipad

---
+++ js/cta.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	if ($('#cta').length) {
+	    var scrollTrigger = 600, // px
+	        backToTop = function () {
+	            var scrollTop = $(window).scrollTop();
+	            if (scrollTop > scrollTrigger) {
+	                $('#cta').addClass('show');
+	            } else {
+	                $('#cta').removeClass('show');
+	            }
+	        };
+	    backToTop();
+	    $(window).on('scroll', function () {
+	        backToTop();
+	    });
+	    $('').on('click', function (e) {
+	        e.preventDefault();
+	        $('html,body').animate({
+	            scrollTop: 0
+	        }, 700);
+	    });
+	}
+
+	$('#cta__close-btn').click(function() {
+		$("#cta").addClass('hide');
+	});
+})
